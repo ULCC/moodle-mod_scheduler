03aa18ed4d03f8f985a3c14264f632b5bea9bea9
Nigel Daley <Nigel.Daley@cosector.com>
2018-10-18 17:53:41 +0100

    added scheduled task file missing from last commit

---
+++ classes/task/clear_waiting_lists.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Scheduled background task for sending automated messages to users on waiting lists
+ *
+ * @package    mod_scheduler
+ * @copyright  2016 Henning Bostelmann and others (see README.txt)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_scheduler\task;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once(dirname(__FILE__).'/../../model/scheduler_instance.php');
+require_once(dirname(__FILE__).'/../../model/scheduler_waiting_list.php');
+require_once(dirname(__FILE__).'/../../model/scheduler_slot.php');
+require_once(dirname(__FILE__).'/../../model/scheduler_appointment.php');
+
+
+/**
+ *  Scheduled background task for sending automated messages to users on waiting lists
+ *
+ * @package    mod_scheduler
+ * @copyright  2016 Henning Bostelmann and others (see README.txt)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class clear_waiting_lists extends \core\task\scheduled_task {
+
+    public function get_name() {
+        return get_string('clearwaitinglistwhenslotsunhidden', 'mod_scheduler');
+    }
+
+    public function execute() {
+        \scheduler_instance::clear_waitinglists($this->get_last_run_time());
+    }
+
+
+
+}
